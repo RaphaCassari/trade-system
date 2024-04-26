018313f6063eff3f577930756581b3d1b96ba25e
Raphael Cassari Benedicto <62524805+RaphaCassari@users.noreply.github.com>
2024-04-26 10:45:45 -0300

    medio *2.5

--- book.py
+++ book.py
@@ -1,5 +1,6 @@
 import MetaTrader5 as mt5
 import time
+import winsound
 # estabelecemos a conexão ao MetaTrader 5
 if not mt5.initialize():
     print("initialize() failed, error code =", mt5.last_error())
@@ -10,26 +11,22 @@ if not mt5.initialize():
 # subscreva para receber atualizações no livro de ofertas para o símbolo DOLK24 (Depth of Market)
 if mt5.market_book_add('DOLK24'):
   # obtemos 10 vezes em um loop os dados do livro de ofertas
-    for i in range(1000):
+    while True:
         # obtemos o conteúdo do livro de ofertas (Depth of Market)
         items = mt5.market_book_get('DOLK24')
-        # exibimos todo o livro de ofertas como uma string tal qual como está
-        # print(items)
-        # agora exibimos cada solicitação separadamente para maior clareza
+
         if items:
             volume_total = 0
             for it in items:
                 volume_total += it.volume
-                # conteúdo da solicitação
-                # print(it._asdict())
-                if it.volume > 300:
-                    print("Volume maior que 300")
-                    import winsound
-                    frequencia = 440
-                    duracao = 4000
-                    winsound.Beep(frequencia, duracao)
+
         volume_medio = volume_total / len(items)
         print(f"Volume total: {volume_total}, Volume médio: {volume_medio}")
+
+        for it in items:
+            if it.volume > volume_medio * 2.5:
+                print(f"Volume acima da média: {it.volume}")
+                winsound.Beep(440, 4000)
         # vamos fazer uma pausa de 5 segundos antes da próxima solicitação de dados do livro de ofertas
         time.sleep(5)
   # cancelamos a subscrição de atualizações no livro de ofertas (Depth of Market)
